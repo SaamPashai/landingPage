3fdec1c0c1f11e15b0ea8f1fd28ad90c2c90d649
Benny Nguyen <benny.nguyenx@gmail.com>
2019-02-02 17:13:46 -0800

    Add title block and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import MenuBar from './MenuBar.js';
+import TitleBlock from './TitleBlock.js';
+import Mission from './Mission.js';
+
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <MenuBar />
+        <TitleBlock />
+        <Mission />
+      </div>
     );
   }
 }
---
+++ src/TitleBlock.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TitleBlock extends Component {
+    render() {
+        return (
+            <div>
+                <div id="titleBlock">
+                    <h1 className="display-3">Project Management for Seattle Public Schools</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TitleBlock;
