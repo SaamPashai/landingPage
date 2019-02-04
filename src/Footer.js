070f5b66e8306bbbac3675fcb61888f7a50586e3
Benny Nguyen <benny.nguyenx@gmail.com>
2019-02-03 17:09:31 -0800

    Made footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return(
-            <footer>
-                
-            </footer>
+            <div id="footerBlock">
+                <h1>Contact Us</h1>
+                <p>If you're interested in our project management tool and wish to implement this
+                   in your project, contact us at the links below!
+                </p>
+
+                <div id="socialMediaContainer">
+                    <i class="fa fa-facebook-f fa-3x" aria-hidden="true"></i>
+                    <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    <a href="mailto:bennyn@uw.edu"><i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i></a>
+                </div>
+            </div>
         );
     }
 }
