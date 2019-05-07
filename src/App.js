e203141242f69f9fc6ece5bcf66a5cd87c94386d
Saam Pashai <doubleapashai@gmail.com>
2019-05-07 15:56:28 -0700

    moved about us down

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
         <MenuBar />
         <TitleBlock />
         <Mission />
-        <AboutUs />
         <Impact />
+        <AboutUs />
         <Footer />
       </div>
     );
